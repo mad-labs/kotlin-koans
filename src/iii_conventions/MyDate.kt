fc3eef911bfebf8ceecd2dd40bea161a18ead678
Gabriele <gabriele.manfredi.999@gmail.com>
2017-09-22 00:46:44 +0200

    modified:   src/iii_conventions/MyDate.kt 	modified:   src/iii_conventions/n26InRange.kt

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -8,7 +8,7 @@ data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparab
 
 }
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
 
 enum class TimeInterval {
     DAY,
@@ -16,4 +16,6 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class DateRange(val start: MyDate, val endInclusive: MyDate){
+    operator fun contains(dateToCheck: MyDate) = start <= dateToCheck && dateToCheck <= endInclusive
+}
