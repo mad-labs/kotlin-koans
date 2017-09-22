0378ad1a0483ada5adfc9750512ce0cedac69e90
Gabriele <gabriele.manfredi.999@gmail.com>
2017-09-23 00:07:06 +0200

    modified:   src/iii_conventions/MyDate.kt 	modified:   src/iii_conventions/n29OperatorsOverloading.kt

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,14 +1,16 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate>{
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
 
     override fun compareTo(other: MyDate): Int {
-        return (this.year*10000+this.month*100+this.dayOfMonth) - (other.year*10000+other.month*100+other.dayOfMonth)
+        return (this.year * 10000 + this.month * 100 + this.dayOfMonth) - (other.year * 10000 + other.month * 100 + other.dayOfMonth)
     }
 
 }
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
+operator fun MyDate.plus(interval: TimeInterval) = this.addTimeIntervals(interval, 1)
+operator fun MyDate.plus(intervals: MultipleTimeIntervals) = this.addTimeIntervals(intervals.interval, intervals.times)
 
 enum class TimeInterval {
     DAY,
@@ -16,17 +18,21 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate): Iterable<MyDate>{
+class MultipleTimeIntervals(val interval: TimeInterval, val times: Int)
+operator fun TimeInterval.times(times: Int) = MultipleTimeIntervals(this, times)
+
+
+class DateRange(val start: MyDate, val endInclusive: MyDate) : Iterable<MyDate> {
     override fun iterator() = DateRangeIterator(this)
 
     operator fun contains(dateToCheck: MyDate) = start <= dateToCheck && dateToCheck <= endInclusive
 }
 
-class DateRangeIterator(val range : DateRange ) : Iterator<MyDate> {
+class DateRangeIterator(val range: DateRange) : Iterator<MyDate> {
 
     var currentDate = range.start
 
-    override operator fun next(): MyDate{
+    override operator fun next(): MyDate {
         val next = currentDate;
         currentDate = currentDate.nextDay()
         return next
